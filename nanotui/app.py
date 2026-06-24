8bdd7160fdca626c21fd5256c1f8f9e3b69eaaf4
michelzwahr <michelzwahr@gmail.com>
2026-06-24 22:06:33 +0200

    feat: quit box before quitting when ESC is pressed

--- nanotui/app.py
+++ nanotui/app.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 import time
 from .screen import clear_screen, hide_cursor, show_cursor
 from .elements import SelectBox, Option
@@ -7,24 +7,16 @@ import tty, termios, select
 def get_key():
     rlist, _, _ = select.select([sys.stdin], [], [], 0.0)
     if not rlist:
-        return None  # Keine Taste gedrückt
-        
-    # 2. Den GESAMTEN Puffer auslesen, der JETZT bereitsteht
-    # Eine Pfeiltaste schickt ihre 3 Zeichen zeitgleich, sie sind also alle hier drin.
+        return None
+
     raw_input = sys.stdin.read(1)
     
-    # Wenn es mit Escape anfängt, schauen wir, was noch im Stream liegt
     if raw_input == '\x1b':
-        # Schau nach, ob sofort weitere Zeichen da sind
         rlist_seq, _, _ = select.select([sys.stdin], [], [], 0.001)
         if rlist_seq:
-            # Es sind Zeichen da! Wir lesen die restlichen (meistens 2, z.B. '[A')
             raw_input += sys.stdin.read(2)
         else:
-            # Es war wirklich nur die isolierte ESC-Taste
             return 'ESC'
-    
-    # 3. Auswertung der gelesenen Zeichenkette
     mapping = {
         '\x1b[A': 'UP',
         '\x1b[B': 'DOWN',
@@ -33,8 +25,6 @@ def get_key():
         '\n': 'ENTER',
         '\r': 'ENTER'
     }
-    
-    # Wenn es im Mapping ist, gib den lesbaren String zurück, ansonsten das Zeichen selbst
     return mapping.get(raw_input, raw_input)
 
 class App:
@@ -46,6 +36,11 @@ class App:
         self.running = True
         self.layer = 0
         self.name = name
+        self.quit_box = SelectBox("Quit?", on_select=self.quit)
+        self.option_quit = Option("Quit", 1)
+        self.option_cancel = Option("Cancel", 0)
+        self.quit_box.add_option(self.option_quit)
+        self.quit_box.add_option(self.option_cancel)
 
     def add_element(self, element: object):
         self.elements.append(element)
@@ -56,6 +51,15 @@ class App:
         if hasattr(element, "select"):
             self.selectable_elements.append(element)
 
+    def remove_element(self, element: object):
+        self.elements.remove(element)
+
+        if hasattr(element, "update"):
+            self.dynamic_elemets.remove(element)
+
+        if hasattr(element, "select"):
+            self.selectable_elements.remove(element)
+
     def change_focus(self, direction: int):
         if not self.selectable_elements: return 
         self.selectable_elements[self.focused_element].on_blur()
@@ -65,8 +69,17 @@ class App:
     def change_layer(self, direction):
         self.layer += direction
 
-    def quit(self):
-        pass
+    def quit(self, value):
+        if value == 1:
+            self.running=False
+        else:
+            if self.quit_box in self.elements:
+                self.change_layer(-1)
+                self.remove_element(self.quit_box)
+                self.draw_all()
+                self.focused_element = 0
+                self.selectable_elements[self.focused_element].on_focus()
+
 
     def draw_all(self):
         for element in self.elements:
@@ -79,29 +92,42 @@ class App:
             import tty, termios
             fd = sys.stdin.fileno()
             old_settings = termios.tcgetattr(fd)
-            tty.setraw(fd)  # Schaltet das gesamte Terminal ab JETZT stumm
+            tty.setraw(fd)
             is_unix = True
         except ImportError:
             pass
 
         try: 
             hide_cursor()
+            if self.selectable_elements and self.layer == 0:
+                self.selectable_elements[self.focused_element].on_focus()
+
+            old_size = []
+
             while self.running:
 
                 for el in self.dynamic_elemets:
                     el.update()
 
-                if self.selectable_elements and self.layer == 0:
-                    self.selectable_elements[self.focused_element].on_focus()
+                
+                current_terminal_size = [os.get_terminal_size().columns, os.get_terminal_size().lines]
+                if current_terminal_size != old_size:
+                    self.draw_all()
+                old_size = [os.get_terminal_size().columns, os.get_terminal_size().lines]
                 
                 key = get_key()
 
                 match(key):
                     case "ESC":
                         if self.layer == 0:
-                            self.running = False
+                            clear_screen()
+                            self.add_element(self.quit_box)
+                            self.quit_box.highlighted_option = self.quit_box.options.index(self.option_cancel)
+                            self.focused_element = self.selectable_elements.index(self.quit_box)
+                            self.layer = 1
+                            self.quit_box.select()
                         else:
-                            self.change_layer(-1)
+                            self.quit(0)
                     case ".":
                         if self.layer == 0:
                             self.change_focus(1)
@@ -119,19 +145,11 @@ class App:
                                 self.change_layer(1)
                             elif self.layer == 1 and hasattr(self.selectable_elements[self.focused_element], "enter"):
                                 self.selectable_elements[self.focused_element].enter()
-
-
                     case _:
-                        # Das fängt alle anderen Tasten (und None) ab, damit nichts passiert
                         pass
                     
-
-
-
                 time.sleep(0.05)
         finally:
-            # Reißleine: Egal was passiert (selbst bei einem Absturz),
-            # wir stellen das Terminal für den User wieder sauber her!
             show_cursor()
             if is_unix:
                 termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
